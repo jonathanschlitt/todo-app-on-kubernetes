37820a2005dc5079e99f6efe67c38bb0568b2d40
jonathanschlitt <development@jonathanschlitt.de>
2023-03-04 15:52:16 +0100

    add docker versions + helm improvements + metrics

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,84 +1,85 @@
 const cassandra = require('cassandra-driver');
-const env = require('dotenv').config();
-let authProvider = new cassandra.auth.PlainTextAuthProvider(process.env.DATABASE_USER, process.env.DATABASE_PASS);
-let CREATE_KEYSPACE_QUERY = "create keyspace " + process.env.DATABASE_KEYSPACE + " with replication = {'class': 'SimpleStrategy', 'replication_factor': 3};"
-
-let CREATE_TODO_TABLE_QUERY = "create table if not exists todo\n" +
-    "(\n" +
-    "    id          uuid primary key,\n" +
-    "    active      boolean,\n" +
-    "    deadline    text,\n" +
-    "    description text,\n" +
-    "    name        text,\n" +
-    "    showdetails boolean,\n" +
-    "    user_id     uuid\n" +
-    ")\n" +
-    "with caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}\n" +
-    "    and compaction = {'class': 'SizeTieredCompactionStrategy'}\n" +
-    "and compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}\n" +
-    "and dclocal_read_repair_chance = 0\n" +
-    "and speculative_retry = '99.0PERCENTILE';"
-
-let CREATE_USER_TABLE_QUERY = "create table if not exists user\n" +
-    "(\n" +
-    "    id       uuid primary key,\n" +
-    "    email    text,\n" +
-    "    lastname text,\n" +
-    "    password text,\n" +
-    "    surname  text\n" +
-    ")\n" +
-    "with caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}\n" +
-    "    and compaction = {'class': 'SizeTieredCompactionStrategy'}\n" +
-    "and compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}\n" +
-    "and dclocal_read_repair_chance = 0\n" +
-    "and speculative_retry = '99.0PERCENTILE';"
-
 const {
-  DATABASE_CONTACT_POINTS, DATABASE_LOCAL_DATACENTER, DATABASE_KEYSPACE, DATABASE_USER, DATABASE_PASS
-} = process.env
+  DATABASE_CONTACT_POINTS,
+  DATABASE_LOCAL_DATACENTER,
+  DATABASE_KEYSPACE,
+  DATABASE_USER,
+  DATABASE_PASS,
+} = process.env;
+const authProvider = new cassandra.auth.PlainTextAuthProvider(
+  DATABASE_USER,
+  DATABASE_PASS
+);
 
-console.log("contactPoint: " + DATABASE_CONTACT_POINTS)
-console.log("datacenter: " + DATABASE_LOCAL_DATACENTER)
-console.log("keyspace: " + DATABASE_KEYSPACE)
-console.log("user: " + DATABASE_USER)
-console.log("pass: " + DATABASE_PASS)
+const CREATE_KEYSPACE_QUERY = `CREATE KEYSPACE IF NOT EXISTS ${DATABASE_KEYSPACE} WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 3};`;
+
+const CREATE_TODO_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS todo (
+    id          UUID PRIMARY KEY,
+    active      BOOLEAN,
+    deadline    TEXT,
+    description TEXT,
+    name        TEXT,
+    showdetails BOOLEAN,
+    user_id     UUID
+) WITH caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}
+  AND compaction = {'class': 'SizeTieredCompactionStrategy'}
+  AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+  AND dclocal_read_repair_chance = 0
+  AND speculative_retry = '99.0PERCENTILE';`;
+
+const CREATE_USER_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS user (
+    id       UUID PRIMARY KEY,
+    email    TEXT,
+    lastname TEXT,
+    password TEXT,
+    surname  TEXT
+) WITH caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}
+  AND compaction = {'class': 'SizeTieredCompactionStrategy'}
+  AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+  AND dclocal_read_repair_chance = 0
+  AND speculative_retry = '99.0PERCENTILE';`;
 
 const cassandraAdminClient = new cassandra.Client({
-    contactPoints: process.env.DATABASE_CONTACT_POINTS.split(','),
-    localDataCenter: process.env.DATABASE_LOCAL_DATACENTER,
-    authProvider: authProvider
+  contactPoints: DATABASE_CONTACT_POINTS.split(','),
+  localDataCenter: DATABASE_LOCAL_DATACENTER,
+  authProvider: authProvider,
 });
 
 const cassandraClient = new cassandra.Client({
-  contactPoints: process.env.DATABASE_CONTACT_POINTS.split(','),
-  localDataCenter: process.env.DATABASE_LOCAL_DATACENTER,
-  keyspace: process.env.DATABASE_KEYSPACE,
-  authProvider: authProvider
+  contactPoints: DATABASE_CONTACT_POINTS.split(','),
+  localDataCenter: DATABASE_LOCAL_DATACENTER,
+  keyspace: DATABASE_KEYSPACE,
+  authProvider: authProvider,
 });
 
-function initDatabase(){
-    cassandraClient.connect(function(e) {
-        if (e !== null) {
-            if (e.toString().includes("Keyspace " + process.env.DATABASE_KEYSPACE + " does not exist")) {
-                console.log("Server is creating needed Keyspace")
-                cassandraAdminClient.connect(function (e) {
-                    cassandraAdminClient.execute(CREATE_KEYSPACE_QUERY, function(e, res) {
-                        console.log("Server created required Keyspace")
-                        cassandraClient.execute(CREATE_USER_TABLE_QUERY, function(e, res) {
-                            console.log("Server created Table User")
-                        });
+const initDatabase = async () => {
+  try {
+    await cassandraClient.connect();
+    console.log(`Connected to Cassandra cluster at ${DATABASE_CONTACT_POINTS}`);
 
-                        cassandraClient.execute(CREATE_TODO_TABLE_QUERY, function(e, res) {
-                            console.log("Server created Table Todo")
-                        });
-                    });
-                })
-            }
-        } else {
-            cassandraClient.execute(CREATE_USER_TABLE_QUERY, function(e, res) {});
-            cassandraClient.execute(CREATE_TODO_TABLE_QUERY, function(e, res) {});
-        }
-    });
-}
+    // Check if keyspace exists, create it if not
+    const keyspaceMetadata = await cassandraClient.execute(
+      `SELECT * FROM system_schema.keyspaces WHERE keyspace_name = '${DATABASE_KEYSPACE}'`
+    );
+    if (keyspaceMetadata.rows.length === 0) {
+      console.log(`Creating keyspace ${DATABASE_KEYSPACE}`);
+      await cassandraAdminClient.connect();
+      await cassandraAdminClient.execute(CREATE_KEYSPACE_QUERY);
+      console.log(`Created keyspace ${DATABASE_KEYSPACE}`);
+    }
 
-module.exports = { cassandraClient, cassandraAdminClient, initDatabase}
+    // Create user table
+    console.log(`Creating table user in keyspace ${DATABASE_KEYSPACE}`);
+    await cassandraClient.execute(CREATE_USER_TABLE_QUERY);
+    console.log(`Created table user in keyspace ${DATABASE_KEYSPACE}`);
+
+    // Create todo table
+    console.log(`Creating table todo in keyspace ${DATABASE_KEYSPACE}`);
+    await cassandraClient.execute(CREATE_TODO_TABLE_QUERY);
+    console.log(`Created table todo in keyspace ${DATABASE_KEYSPACE}`);
+  } catch (error) {
+    console.error(`Error initializing Cassandra database: ${error}`);
+  }
+};
+
+module.exports = { cassandraClient, cassandraAdminClient, initDatabase };
--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,158 +1,146 @@
 const jwt = require('jsonwebtoken');
-const todoRepository = require("../repository/todoRepository");
+const todoRepository = require('../repository/todoRepository');
 const env = require('dotenv').config();
 
 addTodoItem_post = async (req, res) => {
-    let {name, description, active, showDetails, deadline} = req.body;
+  let { name, description, active, showDetails, deadline } = req.body;
 
-    if (showDetails === undefined) {
-        showDetails = true
-    }
+  if (showDetails === undefined) {
+    showDetails = true;
+  }
 
-    let user = await req.user
+  let user = await req.user;
 
-    if (user !== undefined && user != null) {
-        todoRepository.insertToDo(user.id, name, description, active, deadline)
-            .then(
-                todoId => {
-                    return res.status(200).json(
-                        {
-                            _id: todoId.toString(),
-                            name: name,
-                            description: description,
-                            deadline: deadline,
-                            active: active,
-                            showDetails: showDetails
-                        }
-                    )
-                }
-            )
-            .catch(
-                err => {
-                    console.log(err)
-                    return res.status(500).send({
-                        success: false,
-                        message: 'Internal Server error!'
-                    })
-                }
-            )
-    } else {
-
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
+  if (user !== undefined && user != null) {
+    todoRepository
+      .insertToDo(user.id, name, description, active, deadline)
+      .then((todoId) => {
+        return res.status(200).json({
+          _id: todoId.toString(),
+          name: name,
+          description: description,
+          deadline: deadline,
+          active: active,
+          showDetails: showDetails,
         });
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).send({
+          success: false,
+          message: 'Internal Server error!',
+        });
+      });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.',
+    });
+  }
 
-
-    //
-    // if (newsItemCreated) {
-    //   res.status(200).json(todoItem);
-    // } else {
-    //   res.status(500);
-    //   throw new Error('Error creating todoItem!');
-    // }
+  //
+  // if (newsItemCreated) {
+  //   res.status(200).json(todoItem);
+  // } else {
+  //   res.status(500);
+  //   throw new Error('Error creating todoItem!');
+  // }
 };
 
 todoItems_get = async (req, res) => {
+  let user = await req.user;
 
-    let user = await req.user
+  if (user === undefined || user == null) {
+    return res.status(401).send({
+      success: false,
+      message: 'Unauthorized!',
+    });
+  }
 
-    if (user === undefined || user == null) {
-        return res.status(401).send({
-            success: false,
-            message: 'Unauthorized!'
-        })
-    }
-
-    let todos = (await todoRepository.getToDos(user.id)).map(toDoToJson)
-    res.status(200).json(
-        todos
-    )
+  let todos = (await todoRepository.getToDos(user.id)).map(toDoToJson);
+  res.status(200).json(todos);
 };
 
 updateTodoItem_put = async (req, res) => {
+  let user = await req.user;
 
-    let user = await req.user
+  if (user === undefined || user == null) {
+    return res.status(401).send({
+      success: false,
+      message: 'Unauthorized!',
+    });
+  }
 
-    if (user === undefined || user == null) {
-        return res.status(401).send({
-            success: false,
-            message: 'Unauthorized!'
-        })
-    }
+  const id = req.params.id;
 
-    const id = req.params.id;
+  const { name, description, active, showDetails, deadline } = req.body;
 
-    const {name, description, active, showDetails, deadline} = req.body;
+  const updatedTodo = todoRepository.updateToDo(
+    id.toString(),
+    active,
+    deadline,
+    deadline,
+    name,
+    showDetails
+  );
 
-    const updatedTodo = todoRepository.updateToDo(
-        id.toString(),
-        active,
-        deadline,
-        deadline,
-        name,
-        showDetails
-    )
+  const todoItem = {
+    _id: id.toString(),
+    user_id: user.id.toString(),
+    name: name,
+    description: description,
+    active: active,
+    deadline: deadline,
+    showDetails: showDetails,
+  };
 
-
-    const todoItem = {
-        _id: id.toString(),
-        user_id: user.id.toString(),
-        name: name,
-        description: description,
-        active: active,
-        deadline: deadline,
-        showDetails: showDetails,
-    };
-
-    if (updatedTodo) {
-        res.status(201).json(todoItem);
-    } else {
-        res.status(500);
-        throw new Error('Updating data failed!');
-    }
+  if (updatedTodo) {
+    res.status(201).json(todoItem);
+  } else {
+    res.status(500);
+    throw new Error('Updating data failed!');
+  }
 };
 
 deleteTodoItem_delete = async (req, res) => {
-    let user = await req.user
+  let user = await req.user;
 
-    if (user === undefined || user == null) {
-        return res.status(401).send({
-            success: false,
-            message: 'Unauthorized!'
-        })
-    }
+  if (user === undefined || user == null) {
+    return res.status(401).send({
+      success: false,
+      message: 'Unauthorized!',
+    });
+  }
 
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const todoItemDeleted = todoRepository.deleteToDo(id);
+  const todoItemDeleted = todoRepository.deleteToDo(id);
 
-    if (todoItemDeleted) {
-        res.status(200).json(id);
-    } else {
-        res.status(400);
-        throw new Error('todoItem not found in the database');
-    }
+  if (todoItemDeleted) {
+    res.status(200).json(id);
+  } else {
+    res.status(400);
+    throw new Error('todoItem not found in the database');
+  }
 };
 
 function toDoToJson(todo) {
-    return {
-        _id: todo.id.toString(),
-        user_id: todo.user_id.toString(),
-        name: todo.name,
-        description: todo.description,
-        active: todo.active,
-        deadline: todo.deadline,
-        showDetails: todo.showdetails,
-    }
+  return {
+    _id: todo.id.toString(),
+    user_id: todo.user_id.toString(),
+    name: todo.name,
+    description: todo.description,
+    active: todo.active,
+    deadline: todo.deadline,
+    showDetails: todo.showdetails,
+  };
 }
 
 module.exports = {
-    addTodoItem_post,
-    todoItems_get,
-    updateTodoItem_put,
-    deleteTodoItem_delete,
+  addTodoItem_post,
+  todoItems_get,
+  updateTodoItem_put,
+  deleteTodoItem_delete,
 };
